07ee23c730155ad79c796852bc24dc21ccc19510
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-04-27 16:12:13 -0300

    Requisitos Opcionales

--- src/components/SensorCharts.jsx
+++ src/components/SensorCharts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect, useRef, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { 
   Chart as ChartJS, 
@@ -22,6 +22,76 @@ ChartJS.register(
 );
 
 const SensorCharts = ({ historyData }) => {
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState({ stopped: false, running: false, creating: false, stopping: false });
+
+  const fetchServerData = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/c/54c7-e3a3-47b4-9068");
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("Dados recebidos:", data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  };
+
+  const pollingFunc = async () => {
+    try {
+      console.log("Iniciando polling");
+      //fetch server data
+      const data = await fetchServerData();
+     
+      console.log("Status recebido:", data?.status);
+      
+      switch (data?.status) {
+        case "stopped":
+          setStatus({ stopped: true });
+          break;
+    
+        case "running":
+          if (data?.url) {
+            setData(data);
+            setStatus({ running: true });
+          }
+          break;
+    
+        case "creating":
+          setStatus({ creating: true });
+          console.log("Server is creating");
+          break;
+    
+        case "stopping":
+          setStatus({ stopping: true });
+          console.log("Server is stopping");
+          break;
+    
+        default:
+          break;
+      }
+    } catch (error) {
+      console.error("Erro no polling:", error);
+    }
+  };
+
+  const pollingRef = useRef(null);
+  useEffect(() => {
+    const startPolling = () => {
+      pollingRef.current = setInterval(() => {
+        pollingFunc();
+      }, 5000); // Poll every 5 seconds
+    };
+    startPolling();
+
+    return () => {
+      clearInterval(pollingRef.current);
+    };
+  }, []);
+
   const heartRateData = {
     labels: ['6h atrás', '5h atrás', '4h atrás', '3h atrás', '2h atrás', '1h atrás'],
     datasets: [
